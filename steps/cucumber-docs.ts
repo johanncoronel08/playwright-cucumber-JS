6ea9877befe8483e97c5f131e83d9d0cd7cf600b
Gabriel Ramirez <gabriel.ramirez.lemus@gmail.com>
2024-11-26 00:49:11 -0300

    se crean pasos extras

--- steps/cucumber-docs.ts
+++ steps/cucumber-docs.ts
@@ -1,8 +1,7 @@
-// @ts-check
 
 import { Given, When, Then } from '@cucumber/cucumber';
 import CustomWorld from './../config/world'
-import { expect, Page } from '@playwright/test';
+import { expect } from '@playwright/test';
 import { CucumberPage } from '../pages/CucumberPage';
 
 
@@ -29,10 +28,20 @@ Then("valido que estoy en Docs", async function () {
 
 })
 
-When("busco seccion {string}", { timeout: 10000 }, async function (this: CustomWorld, variableReport) {
+When("busco seccion {string}", { timeout: 15000 }, async function (this: CustomWorld, variableReport:string) {
     //(//input[@placeholder='How can we help?'])[1]
     const inputSeccion = await this.page.locator("xpath=(//input[@placeholder='How can we help?'])[1]")
     await expect(inputSeccion).toBeEnabled();
     await inputSeccion.fill(variableReport)
     await this.page.waitForTimeout(5000)
+    await inputSeccion.press("ArrowDown")
+    await this.page.waitForTimeout(1000)
+    await inputSeccion.press("Enter")
+    await this.page.waitForTimeout(4000)
+})
+
+Then('valido que estoy en {string}', {timeout:10000},async function(this: CustomWorld,url: string) {
+  // Write code here that turns the phrase above into concrete actions
+  await expect(this.page).toHaveURL(url);
+  await this.page.waitForTimeout(5000);
 })
